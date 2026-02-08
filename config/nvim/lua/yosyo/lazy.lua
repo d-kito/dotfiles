6afb4b980a05301f9125a645a22431bec38016b8
dak <160011363+d-kito@users.noreply.github.com>
2026-02-08 12:17:46 +0100

    Add files via upload

--- config/nvim/lua/yosyo/lazy.lua
+++ config/nvim/lua/yosyo/lazy.lua
@@ -73,4 +73,27 @@ require('lazy').setup({
     ---@type render.md.UserConfig
     opts = {},
     },
+	{
+		'windwp/nvim-autopairs',
+		event = "InsertEnter",
+		config = true
+		-- use opts = {} for passing setup options
+		-- this is equivalent to setup({}) function
+	},
+	{
+		'numToStr/Comment.nvim',
+		opts = {
+			-- add any options here
+		}
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end
+	}
 })
